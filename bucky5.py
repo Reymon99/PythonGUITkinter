f97c2adace94cba4c183f91962e54cb045ce8ec8
Reymon99 <s.maje@udla.edu.co>
2018-08-10 00:57:42 -0500

    Menus, Toolbar and Statusbar

--- bucky5.py
+++ bucky5.py
@@ -13,10 +13,26 @@ menu.add_cascade(label="File",menu=subMenu)
 subMenu.add_command(label="New Project...",command=doNothing)
 subMenu.add_command(label="Now...",command=doNothing)
 subMenu.add_separator()
-subMenu.add_command(label="Exit",command=doNothing)
+subMenu.add_command(label="Exit",command=root.quit)
 
 editMenu=Menu(menu)
 menu.add_cascade(label="Edit",menu=editMenu)
+editMenu.add_command(label="Redo",command=doNothing)
 
+#Toolbar
+
+toolbar=Frame(root,bg="blue")
+
+insertButt=Button(toolbar,text="Insert Image",command=doNothing)
+insertButt.pack(side=LEFT,padx=2,pady=2)
+printButt=Button(toolbar,text="Print",command=doNothing)
+printButt.pack(side=LEFT,padx=1,pady=1)
+
+toolbar.pack(side=TOP,fill=X)
+
+#Statusbar
+
+status=Label(root,text="Preparing to do nothing...",bd=1,relief=SUNKEN,anchor=W)
+status.pack(side=BOTTOM,fill=X)
 
 root.mainloop()
